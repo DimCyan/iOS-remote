57d95a62597f249b6f3ef4806da6bb15ca88d0b2
SimonWDC <43487601+SimonWDC@users.noreply.github.com>
2021-09-06 14:52:48 +0800

    change route and console format

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from flask_cors import *
+import json
+
+app = Flask(__name__)
+CORS(app, support_crenditals=True)
+
+
+@app.route('/')
+def index():
+    return render_template('mousedrag.html')
+
+
+@app.route('/click', methods=['POST'])
+def click():
+    data = json.loads(request.form.get('data'))
+    disX = data['disX']
+    disY = data['disY']
+    print('click: ({}, {})'.format(disX, disY))
+    return "click it"
+
+
+@app.route('/drag', methods=['POST'])
+def drag():
+    data = json.loads(request.form.get('data'))
+    disX = data['disX']
+    disY = data['disY']
+    toX = data['toX']
+    toY = data['toY']
+    print('click: ({}, {}) and drag to: ({}, {})'.format(disX, disY, toX, toY))
+    return "click and drag it"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
