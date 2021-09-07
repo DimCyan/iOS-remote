b7ec47c1e1653afb0e68c0e45083b408e843a429
SimonWDC <43487601+SimonWDC@users.noreply.github.com>
2021-09-07 14:14:07 +0800

    version 1.0

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from flask_cors import *
 import json
+import wda
+from logzero import logger
 
 app = Flask(__name__, template_folder='static')
 CORS(app, support_crenditals=True)
@@ -14,30 +16,66 @@ def index():
 @app.route('/click', methods=['POST'])
 def click():
     data = json.loads(request.form.get('data'))
-    disX = data['disX']
-    disY = data['disY']
-    print('click: ({}, {})'.format(disX, disY))
+    disX = float(data['disX'])
+    disY = float(data['disY'])
+    client.click(disX, disY)
+    logger.info('click: ({}, {})'.format(disX, disY))
     return "click it"
 
 
 @app.route('/drag', methods=['POST'])
 def drag():
     data = json.loads(request.form.get('data'))
-    disX = data['disX']
-    disY = data['disY']
-    toX = data['toX']
-    toY = data['toY']
-    print('click: ({}, {}) and drag to: ({}, {})'.format(disX, disY, toX, toY))
+    disX = float(data['disX'])
+    disY = float(data['disY'])
+    toX = float(data['toX'])
+    toY = float(data['toY'])
+    client.swipe(x1=disX, y1=disY, x2=toX, y2=toY, duration=0)
+    logger.info('click: ({}, {}) and drag to: ({}, {})'.format(disX, disY, toX, toY))
     return "click and drag it"
+
+
+@app.route('/home', methods=['POST'])
+def home():
+    client.home()
+    logger.info('Back to home')
+    return "back to home"
+
+
+@app.route('/lock', methods=['POST'])
+def lock():
+    client.lock()
+    logger.info('Press lock button to lock/unlock')
+    return "press lock button"
+
+
+@app.route('/rotation', methods=['POST'])
+def rotation():
+    orientation = client.orientation
+    if orientation == 'PORTRAIT':
+        client.orientation = 'LANDSCAPE'
+    else:
+        client.orientation = 'PORTRAIT'
+    logger.info('Switch orientation')
+    return "switch orientation"
 
 
 @app.route('/send', methods=['POST'])
 def send():
     data = json.loads(request.form.get('data'))
     content = data['text']
-    print('send text content: {}'.format(content))
+    client.send_keys(content)
+    logger.info('send text content: {}'.format(content))
     return "send text"
 
 
 if __name__ == '__main__':
+    with open('device.json', 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+        udid = json_data.get('udid')
+        wda_bundle_id = json_data.get('wda_bundle_id')
+    client = wda.USBClient(
+        udid=udid,
+        port=8100,
+        wda_bundle_id=wda_bundle_id)
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,23 @@
 window.onload = function () {
     var div1 = document.getElementById("remote");
-    var eleH = div1.offsetHeight;
-    var eleW = div1.offsetWidth;
-    console.log('H: ' + eleH + ' W:' + eleW)
     var disX = disY = 0;
 
     div1.onmousedown = function (e) {
         var evnt = e || event;
+        var eleH = div1.offsetHeight;
+        var eleW = div1.offsetWidth;
+        console.log('H: ' + eleH + ' W:' + eleW)
         disX = evnt.clientX - div1.offsetLeft;
-        disY = evnt.clientY - div1.offsetTop;
+        disY = evnt.offsetY - div1.offsetTop;
 
         div1.onmousemove = function (e) {
             var evnt = e || event;
             var x = evnt.clientX - div1.offsetLeft;
-            var y = evnt.clientY - div1.offsetTop;
+            var y = evnt.offsetY - div1.offsetTop;
 
             div1.onmouseup = function () {
                 if (x || y) {
-                    var data = data = {
+                    var data = {
                         data: JSON.stringify({
                             'disX': (disX / eleW).toFixed(2),
                             'disY': (disY / eleH).toFixed(2),
@@ -40,7 +40,7 @@ window.onload = function () {
 
         div1.onmouseup = function () {
             if (disX || disY) {
-                var data = data = {
+                var data = {
                     data: JSON.stringify({
                         'disX': (disX / eleW).toFixed(2),
                         'disY': (disY / eleH).toFixed(2)
@@ -62,7 +62,7 @@ window.onload = function () {
     };
     $(".send").click(function(){
         var content = $("#main-send").val();
-        var data = data = {
+        var data = {
             data: JSON.stringify({
                 'text': content,
             }),
@@ -74,5 +74,26 @@ window.onload = function () {
             data: data,
             dataType: 'json',
         })
-    })
+    });
+    $(".home").click(function(){
+        console.log('click home buttom')
+        $.ajax({
+            url: 'http://localhost:5000/home',
+            type: 'POST',
+        })
+    });
+    $(".lock").click(function(){
+        console.log('Press lock button')
+        $.ajax({
+            url: 'http://localhost:5000/lock',
+            type: 'POST',
+        })
+    });
+    $(".rotation").click(function(){
+        console.log('switch orientation')
+        $.ajax({
+            url: 'http://localhost:5000/rotation',
+            type: 'POST',
+        })
+    });
 };
